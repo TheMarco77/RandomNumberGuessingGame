22703f0f56381b579e5faf157033da3d94a31be6
Marco Ramos <mfilipe@gmail.com>
2019-03-27 09:37:20 -0400

    comments

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// Fun game
+/*
+The Random Number Guessing Game Generates
+a number between 1 and 6 and gives a 
+player two attempts to guess the number right.   
+*/
+
+// assuming the player guessed it wrong the first time
 var correctGuess = false;
+
+// generate a random number from 1 to 6
 var randomNumber = Math.floor(Math.random() * 6 ) + 1;
+
 var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
+
+/*
+test to see if player is
+1. correct
+2. guessed too high
+3. guessed too low
+*/
 if (parseInt(guess) === randomNumber ) {
   correctGuess = true;
 } else if ( parseInt(guess) < randomNumber) { 
@@ -15,6 +31,8 @@ if (parseInt(guess) === randomNumber ) {
     correctGuess = true;
   }
 }
+
+//test if player is correct and output response
 if ( correctGuess ) {
     document.write('<p>You guessed the number!</p>');
 } else {

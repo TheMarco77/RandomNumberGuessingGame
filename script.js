3929a135a175239cd35ffcb5a9bc7c14d737012e
Marco Ramos <mfilipe@gmail.com>
2019-03-27 16:19:07 -0400

    comments1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
 The Random Number Guessing Game Generates
 a number between 1 and 6 and gives a 
-player two attempts to guess the number right.   
+player two attempts to guess the number right. 
 */
 
 // assuming the player guessed it wrong the first time
